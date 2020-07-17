352d43dc1213a33c7c27661c9e2c5cba5ff2baa3
Jardier <jardier@hotmail.com>
2020-07-17 11:50:15 -0300

    Adicionando menu

--- app/src/main/java/com/android/listadetarefas/activity/AdicionarTarefaActivity.kt
+++ app/src/main/java/com/android/listadetarefas/activity/AdicionarTarefaActivity.kt
@@ -2,6 +2,9 @@ package com.android.listadetarefas.activity.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import com.android.listadetarefas.R
 
 class AdicionarTarefaActivity : AppCompatActivity() {
@@ -9,5 +12,21 @@ class AdicionarTarefaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_adicionar_tarefa)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_adicionar_tarefa, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when(item.itemId) {
+            R.id.itemSalvar -> {
+                Toast.makeText(applicationContext,"Menu Salvar clicado", Toast.LENGTH_LONG).show();
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
